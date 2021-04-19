76d3cbda95f0e421e7676dee8c6e1183c057f085
Saleh Omar <salehomar82@gmail.com>
2021-04-19 14:24:31 +0200

    Added functionality

--- js/frameScript.js
+++ js/frameScript.js
@@ -17,29 +17,44 @@ function fetchDataAndUpdateFrame(section) {
     collectionRef.doc(section).get().then(function(doc) {
         if (doc && doc.exists) {
             let myData = doc.data();
+            $("#firstChoice").css('visibility', 'hidden');
+            $("#secondChoice").css('visibility', 'hidden');
+            $(".secondary").css('visibility', 'hidden');
+            $(".main").css('visibility', 'hidden');
+            $("#storyBox").css('visibility', 'hidden');
             $("#gameBody").css("backgroundImage", "url(images/" + myData.background + ")");
-            $("#firstChoice").data("next", myData.first[1]);
-            $("#secondChoice").data("next", myData.second[1]);
-            if (myData.main) {
-                $(".primaryContent").show;
-                $(".primaryContent").html(myData.main);
-            } else { $(".primaryContent").hide(); }
-            if (myData.secondary) {
-                $(".secondaryContent").show;
-                $(".secondaryContent").html(myData.secondary);
-            } else { $(".secondaryContent").hide(); }
-            if (myData.first[0]) {
-                $("#firstChoice").show;
+            if (myData.first) {
+                $("#firstChoice").click(function() {
+                    fetchDataAndUpdateFrame(myData.first[1]);
+                });
                 $("#firstChoice").html(myData.first[0]);
-            } else { $("#firstChoice").hide(); }
-            if (myData.second[0]) {
-                $("#secondChoice").show;
-                $("#secondChoice").html(myData.second[0]);
-            } else { $("#secondChoice").hide(); }
+                $("#firstChoice").css('visibility', 'visible');
+                if (myData.second) {
+                    $("#secondChoice").click(function() {
+                        fetchDataAndUpdateFrame(myData.second[1]);
+                    });
+                    $("#secondChoice").html(myData.second[0]);
+                    $("#secondChoice").css('visibility', 'visible');
+                }
+            }
+            if (myData.main) {
+                $(".primaryContent").html(myData.main);
+                $(".main").css('visibility', 'visible');
+            }
+            if (myData.secondary) {
+                $(".secondaryContent").html(myData.secondary);
+                $(".secondary").css('visibility', 'visible');
+            }
             if (myData.story) {
-                $("#storyBox").show;
                 $("#storyBox").html(myData.story);
-            } else { $("#storyBox").hide(); }
+                $("#storyBox").css('visibility', 'visible');
+            }
+            if (myData.time) {
+                $('#gameBody').fadeTo(myData.time * 1000, 0, function() {
+                    $('#gameBody').css("opacity", 1);
+                    fetchDataAndUpdateFrame(myData.next);
+                });
+            }
         }
     }).catch(function(error) {
         console.log(error);
@@ -48,10 +63,4 @@ function fetchDataAndUpdateFrame(section) {
 
 $(document).ready(function() {
     fetchDataAndUpdateFrame("0");
-    $("#firstChoice").click(function() {
-        fetchDataAndUpdateFrame($("#firstChoice").data("next"));
-    });
-    $("#secondChoice").click(function() {
-        fetchDataAndUpdateFrame($("#secondChoice").data("next"));
-    });
 });
