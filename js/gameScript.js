7186245d93b2a74356ef906475137f3db66a5e41
funzamilia <salehomar82@gmail.com>
2024-12-28 21:52:04 +0100

    clean repo

--- js/gameScript.js
+++
@@ -1,8 +0,0 @@
-$(document).on('keydown', function() {
-    $("#titleSong")[0].play();
-});
-$(document).ready(function() {
-    $(".crawl").bind('oanimationend animationend webkitAnimationEnd', function() {
-        window.location.replace("game.html");
-    });
-});
